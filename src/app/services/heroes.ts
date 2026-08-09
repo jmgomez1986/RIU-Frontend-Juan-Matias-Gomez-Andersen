58b821f3b04b881d9d97e25510e3953e4f122f07
Matias Gomez <jmgametal@gmail.com>
2026-08-09 00:45:36 -0300

    Se istala Tailwind. Se realiza el maquetado de una card con informacion de un heroe. Se completa la visualizacion de los heroes totales de la db. Se agrego busqueda paginada, haciendo uso de otro nuevo endpoint que recibe los items por pagina y la pagina, para esto se impemento 'resource' correspondiente a la nueva metodologia reactiva de angular.Tambien se agrego Angular Material para el maquetado y uso de sus componentes

--- src/app/services/heroes.ts
+++ src/app/services/heroes.ts
@@ -1,8 +1,8 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
 
-import { Hero } from '../interfaces/heroes.interface';
+import { Hero, HeroesResponsePaginated } from '../interfaces/heroes.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -11,8 +11,23 @@ export class HeroesService {
   private http = inject(HttpClient);
   private apiUrl = '/api/heroes';
 
+  // Devuelve todos los heroes de la db
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.apiUrl);
+    return this.http
+      .get<Hero[]>(this.apiUrl)
+      .pipe(map((heroes) => heroes.map((hero) => ({ ...hero, image: `images/${hero.image}` }))));
   }
 
+  // Devuelve los heroes de la db paginados
+  getHeroPaginated(page: number, itemsPerPage: number): Observable<HeroesResponsePaginated> {
+    const queryParams: HttpParams = new HttpParams()
+      .set('_page', page.toString())
+      .set('_per_page', itemsPerPage.toString());
+    return this.http.get<HeroesResponsePaginated>(`${this.apiUrl}?${queryParams}`).pipe(
+      map((resp) => ({
+        ...resp,
+        data: resp.data.map((hero) => ({ ...hero, image: `images/${hero.image}` })),
+      })),
+    );
+  }
 }
