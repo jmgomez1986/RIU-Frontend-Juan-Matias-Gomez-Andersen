49d7e8c12e6a2bb9367837bfa790f13db61c499a
Matias Gomez <jmgametal@gmail.com>
2026-08-13 20:01:11 -0300

    Se crea una directiva para que al escrbir el nombre del heroe (en edicio o creando uno nuevo) siempre se transforme a mayusculas

---
+++ src/app/directives/transform-text-uppercase.spec.ts
@@ -0,0 +1,8 @@
+import { TransformTextUppercase } from './transform-text-uppercase';
+
+describe('TransformTextUppercase', () => {
+  it('should create an instance', () => {
+    const directive = new TransformTextUppercase();
+    expect(directive).toBeTruthy();
+  });
+});
